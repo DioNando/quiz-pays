b90fddc6233932bea6b5ce0a1803272b63e26d4c
David Fernando <mahefa.fernando@gmail.com>
2023-12-20 16:30:56 +0100

    Feature : API

---
+++ app/Http/Resources/PartieResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PartieResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'pseudo' => $this->pseudo,
+            'score' => $this->score,
+            'date' => date_format($this->created_at, "d-m-Y"),
+        ];
+    }
+}
---
+++ database/factories/PartieFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partie>
+ */
+class PartieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'pseudo' => fake()->userName(),
+            'score' => rand(0,200),
+        ];
+    }
+}
